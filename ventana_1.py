71449c189ae9a2d15f803b1fc623a935d2a0165c
penguindavid <davidmacias@colegioguanenta.edu.com>
2025-07-31 16:03:22 -0500

    h

---
+++ ventana_1.py
@@ -0,0 +1,25 @@
+# Se importa la libreria tkinter con todas sus funciones 
+from tkinter import *
+
+# --------------------------------- 
+# Funciones de la app
+# --------------------------------- 
+
+
+# --------------------------------- 
+# Ventana principal de la app
+# --------------------------------- 
+
+# Se declara una variable llamada ventana principal, que adquiere las caracteristicas de un objeto tipo Tk ()
+
+ventana_principal = Tk()
+
+# Titulo de la ventana
+ventana_principal.title("David Santiago Macías Maldonado")
+
+# Tamaño de la ventana principal
+ventana_principal.geometry("800x500")
+
+# Metodo principal que despliega la ventana en pantalla
+
+ventana_principal.mainloop()
